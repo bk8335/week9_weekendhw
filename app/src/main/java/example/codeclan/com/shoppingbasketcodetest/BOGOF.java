ee785395a2e5c131345951505ddbf15177cc6b16
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-08 08:12:06 +0100

    wrote in notes proper BOGOF method

--- app/src/main/java/example/codeclan/com/shoppingbasketcodetest/BOGOF.java
+++ app/src/main/java/example/codeclan/com/shoppingbasketcodetest/BOGOF.java
@@ -21,6 +21,8 @@ public class BOGOF implements Discountable {
     // as a basic v1, if you just add everything to a hashmap, then a 2nd identical title will simply replace the 1st one, making it BOGOF
     // use the .value method to sum up the value of the basket in the hashmap
 
+
+//    means you can only add two of each item, and to make it work, rename title for a 3rd/4th/5th one etc.
     public HashMap<String, Double> basketToHash(ArrayList<Item> basket) {
         for (Item item : basket) {
             basketHash.put(item.getTitle(), item.getPrice());
@@ -36,17 +38,10 @@ public class BOGOF implements Discountable {
         return basketHashValue;
     }
 
-//    public void buyOneGetOneFree(Item itemBeingAdded) {
-//        ArrayList<Item> basketClone = new ArrayList<>();
-//        for (Item item : basket){
-//            if(itemBeingAdded != item) {
-//                basketClone.add(itemBeingAdded);
-//            }
-//            else
-//                itemBeingAdded.setPrice(0);
-//                basketClone.add(itemBeingAdded);
-//        }
-//    }
+//    for a proper BOGOF, need a hashmap with the count of each unique key.
+//    With the value of each key starting at 1, and then going up each time it reappears
+//    divide the number by two, multiply that by the price to get the value.
+//    if number / 2 remainder 1 i.e. odd, then add one of the price to the value above.
 
 
 }
